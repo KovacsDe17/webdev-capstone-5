f7bce34f9d3e9ea329863423b6f328ca90dbae30
Daniel Kovacs <kovacs.de17@gmail.com>
2024-04-03 06:58:06 +0200

    Initial commit

---
+++ SQL/Queries.sql
@@ -0,0 +1,22 @@
+-- Create tables --
+create table books (
+    ISBN int not null primary key,
+    title varchar(50) not null,
+    author varchar(50) not null
+);
+
+create table reviews (
+    id serial not null primary key,
+    book_isbn int not null references books(ISBN),
+    read_date date not null,
+    notes text not null,
+    rating int not null,
+);
+
+-- Create data --
+
+-- Read data --
+
+-- Update data --
+
+-- Delete data --
