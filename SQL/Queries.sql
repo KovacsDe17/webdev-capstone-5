9e5a3a0bf577384a0cab0f294a46aaef03c49dc2
Daniel Kovacs <kovacs.de17@gmail.com>
2024-04-05 16:25:22 +0200

    Added data persistance

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -1,23 +1,24 @@
 -- Create tables --
-create table books (
-    ISBN int not null primary key,
-    title varchar(50) not null,
-    author varchar(50) not null,
-    description text not null
-);
-
-create table reviews (
-    id serial not null primary key,
-    book_isbn int not null references books(ISBN),
-    read_date date not null,
-    notes text not null,
-    rating int not null,
+CREATE TABLE bookreviews (
+    isbn bigint NOT NULL PRIMARY KEY,
+    title varchar(50) NOT NULL,
+    author varchar(50) NOT NULL,
+    description text NOT NULL,    
+    read_date date NOT NULL,
+    notes text NOT NULL,
+    rating int NOT NULL
 );
 
 -- Create data --
+INSERT INTO bookreviews (isbn, title, author, description, read_date, notes, rating) VALUES ($1, $2, $3, $4, $5, $6, $7);
 
 -- Read data --
+SELECT * FROM bookreviews ORDER BY read_date DESC;
+SELECT * FROM bookreviews ORDER BY rating DESC;
+SELECT * FROM bookreviews ORDER BY title ASC;
 
 -- Update data --
+UPDATE bookreviews SET title=$2, author=$3, description=$4, read_date=$5, notes=$6, rating=$7 WHERE isbn = $1;
 
 -- Delete data --
+DELETE FROM bookreviews WHERE isbn = $1;
--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import express from "express";
 import bodyParser from "express";
-import axios from "axios";
 import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "Kovacsde17",
+    database: "booknotes"
+});
+
+db.connect();
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    let sortby = req.query.sortby ?? "title";
+    let orderAsc = req.query.order === "desc" ? false : true;
+
+    bookReviews = await loadAllReviews(sortby, orderAsc);
+
     res.render("index.ejs", {bookreviews: bookReviews});
 });
 
@@ -31,61 +45,42 @@ app.get("/book/:isbn", (req, res) => {
     res.render("book.ejs", {book: book});
 });
 
-app.post("/add", (req, res) => {
-    try {
-        const book = {
-            isbn: req.body.isbn,
-            title: req.body.title,
-            author: req.body.author,
-            description: req.body.description,
-            readDate: req.body.readDate,
-            notes: req.body.notes,
-            rating: req.body.rating
-        };
-    
-        bookReviews.push(book);
+app.post("/add", async (req, res) => {
+    const review = {
+        isbn: req.body.isbn,
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        read_date: req.body.read_date,
+        notes: req.body.notes,
+        rating: req.body.rating
+    };
 
-        console.log("Book review created successfully!");
-    } catch (error) {
-        console.error("Error was caught during creation! " + error.stack);
-    }
+    await addReview(review);
+    
+    res.redirect("/");
+});
+
+app.post("/edit/:isbn", async (req, res) => {
+    const review = {
+        isbn: req.params.isbn,
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        read_date: req.body.read_date,
+        notes: req.body.notes,
+        rating: req.body.rating
+    };
+
+    await updateReview(review);
 
     res.redirect("/");
 });
 
-app.post("/edit/:isbn", (req, res) => {
-    try {
-        const book = {
-            isbn: req.params.isbn,
-            title: req.body.title,
-            author: req.body.author,
-            description: req.body.description,
-            readDate: req.body.readDate,
-            notes: req.body.notes,
-            rating: req.body.rating
-        };
-
-        const index = bookReviews.findIndex((searchedBook) => searchedBook.isbn == book.isbn);
-        bookReviews[index] = book;
-        console.log("Edited book review successfully!");
-    } catch (error) {
-        console.error("Error was caught during edit! " + error.stack);
-    }
-
-    res.redirect("/");
-});
-
-app.post("/delete/:isbn", (req, res) => {
-    try {
-        const isbn = req.params.isbn;
+app.post("/delete/:isbn", async (req, res) => {
+    const isbn = req.params.isbn;
+    await deleteReview(isbn);
     
-        const index = bookReviews.findIndex((searchedBook) => searchedBook.isbn == isbn);
-        bookReviews.splice(index, 1);
-        console.log("Deleted book review successfully!");
-    } catch (error) {
-        console.error("Error was caught during delete! " + error.stack);
-    }
-
     res.redirect("/");
 });
 
@@ -93,8 +88,56 @@ app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
 
+async function loadAllReviews(orderby="title", asc = true){
+    if(orderby != "title" && orderby != "rating" && orderby != "read_date"){
+            orderby = "title";
+        }
+
+    let reviews = [];
+
+    try {
+        const result = await db.query(`SELECT * FROM bookreviews ORDER BY ${orderby} ${asc?"ASC":"DESC"}`);
+        result.rows.forEach((review) => {
+            let newDate = review.read_date.toISOString().slice(0, 10);
+            review.read_date = newDate;
+            reviews.push(review);
+        });
+    } catch (error) {
+        console.error("Error executing query! ", error.stack);
+    }
+
+    return reviews;
+}
+
+async function addReview(review){
+    try {
+        const result = await db.query("INSERT INTO bookreviews (isbn, title, author, description, read_date, notes, rating) VALUES ($1, $2, $3, $4, $5, $6, $7);",
+            [review.isbn, review.title, review.author, review.description, review.read_date, review.notes, review.rating]);
+    } catch (error) {
+        console.error("Error executing query! ", error.stack);
+    }
+}
+
+async function updateReview(review){
+    try {
+        const result = await db.query("UPDATE bookreviews SET title=$1, author=$2, description=$3, read_date=$4, notes=$5, rating=$6 WHERE isbn = $7;",
+            [review.title, review.author, review.description, review.read_date, review.notes, review.rating, review.isbn]);
+        console.log("Updated review! " + result.rows);
+    } catch (error) {
+        console.error("Error executing query! ", error.stack);
+    }
+}
+
+async function deleteReview(isbn){
+    try {
+        const result = await db.query("DELETE FROM bookreviews WHERE isbn = $1", [isbn]);
+    } catch (error) {
+        console.error("Error executing query! ", error.stack);
+    }
+}
+
 //Mock data
-const bookReviews = [
+var bookReviews = [
     {
         isbn: 9780316452465,
         title: "Vaják I. - The Witcher - Az utolsó kívánság",
@@ -103,7 +146,7 @@ const bookReviews = [
         Hiába tűnik azonban valami gonosznak vagy jónak, nem biztos, hogy valóban az - és minden mesében van egy csipetnyi igazság.
         
         Andrzej Sapkowski 1948-ban született Lengyelországban. Hazája egyik leghíresebb és legsikeresebb szerzőjének számít. A World Fantasy Életműdíjjal kitüntetett szerző Vaják-sorozatával nemzetközi sikert aratott, könyvei alapján képregények és számítógépes játékok is készültek, 2019-ben pedig a Netflix forgatott belőlük sorozatot Henry Cavill főszereplésével.`,
-        readDate: "2022-01-01",
+        read_date: "2022-01-01",
         notes: "Nincs véleményem.",
         rating: 8
     }
